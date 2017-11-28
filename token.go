34c7f3b019af7f89265051c47a4f88da343fb5cb
lucBoillat <luc.boillat@outlook.com>
2017-11-28 14:38:55 +0100

    Refactoring and added template for configTx

--- token.go
+++ token.go
@@ -13,8 +13,8 @@ func CreateToken() http.Cookie {
   mySigningKey := []byte("secret")
   tokenName := "AuthToken"
 
-  expireToken := time.Now().Add(time.Minute * 1).Unix()
-  expireCookie := time.Now().Add(time.Minute * 1)
+  expireToken := time.Now().Add(time.Minute * 10).Unix()
+  expireCookie := time.Now().Add(time.Minute * 10)
   token := jwt.New(jwt.SigningMethodHS256)
   Claims := token.Claims.(jwt.MapClaims)
   Claims["admin"] = true

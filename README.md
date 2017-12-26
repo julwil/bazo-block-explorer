28756c278ffce78325cb8284ad96d05ffdca8c51
lucBoillat <luc.boillat@outlook.com>
2017-12-26 15:16:48 +0100

    input validation for configtxs

--- README.md
+++ README.md
@@ -3,15 +3,10 @@
   * refactor adminfunc
   * better error handling (not panic)
   * open transactions
-* JS login (client side)
-  * User enters private key
-  * encode JWT using private key
-  * store token in Cookie
-  * Save cookie
-* Login (server side)
-  * User sends request with Cookie
-  * decode JWT using public key (from where public keys?)
-  * Grant access to route
-* Send TX via JS (client side)
-  * make all transactions
 * Database Component
+
+
+TODAY
+* login
+* system parameters
+* statistical information begin
--- static/src/adminjs.js
+++ static/src/adminjs.js
@@ -2,6 +2,9 @@
 var app = new Vue({
   el: '#parameter-list',
   methods:{
+    isPositiveInt: function(value) {
+      return value >>> 0 === parseFloat(value);
+    },
     closeModal: function () {
       $('#myModal').modal('hide');
     },
@@ -20,103 +23,113 @@ var app = new Vue({
     },
     changeBlockSize: function(blocksize, fee) {
       console.log("Changing blocksize: ", blocksize, fee)
-
-      axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
-        function(response) {
-          console.log(response.data)
-          app.accountinfo.txcount = response.data.txCnt
-          if (response.data.isRoot) {
-            axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${1}/${blocksize.blocksize}/${app.blocksize.fee}/${app.accountinfo.txcount}`).then(
-              function(response) {
-                $("#myModal").modal()
-                console.log(response.data);
-                app.accountinfo.txhash = response.data.content[0].detail
-              }
-            )
+      if (app.isPositiveInt(blocksize) && app.isPositiveInt(fee)) {
+        axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
+          function(response) {
+            console.log(response.data)
+            app.accountinfo.txcount = response.data.txCnt
+            if (response.data.isRoot) {
+              axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${1}/${app.blocksize.blocksize}/${app.blocksize.fee}/${app.accountinfo.txcount}`).then(
+                function(response) {
+                  $("#myModal").modal()
+                  console.log(response.data);
+                  app.accountinfo.txhash = response.data.content[0].detail
+                }
+              )
+            }
           }
-        }
-      )
-
+        )
+      } else {
+        alert("Values are not positive integers!");
+      }
     },
     changeDiffInterval: function(diffinterval, fee) {
       console.log("Changing difficulty interval: ", diffinterval, fee)
-
-      axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
-        function(response) {
-          console.log(response.data)
-          app.accountinfo.txcount = response.data.txCnt
-          if (response.data.isRoot) {
-            axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${2}/${diffinterval.diffinterval}/${app.diffinterval.fee}/${app.accountinfo.txcount}`).then(
-              function(response) {
-                $("#myModal").modal()
-                console.log(response.data);
-                app.accountinfo.txhash = response.data.content[0].detail
-              }
-            )
+      if (app.isPositiveInt(diffinterval) && app.isPositiveInt(fee)) {
+        axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
+          function(response) {
+            console.log(response.data)
+            app.accountinfo.txcount = response.data.txCnt
+            if (response.data.isRoot) {
+              axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${2}/${app.diffinterval.diffinterval}/${app.diffinterval.fee}/${app.accountinfo.txcount}`).then(
+                function(response) {
+                  $("#myModal").modal()
+                  console.log(response.data);
+                  app.accountinfo.txhash = response.data.content[0].detail
+                }
+              )
+            }
           }
-        }
-      )
-
+        )
+      } else {
+        alert("Values are not positive integers!");
+      }
     },
     changeMinFee: function(minfee, fee) {
       console.log("Changing minimum fee: ", minfee, fee)
-
-      axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
-        function(response) {
-          console.log(response.data)
-          app.accountinfo.txcount = response.data.txCnt
-          if (response.data.isRoot) {
-            axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${3}/${minfee.minfee}/${app.minfee.fee}/${app.accountinfo.txcount}`).then(
-              function(response) {
-                $("#myModal").modal()
-                console.log(response.data);
-                app.accountinfo.txhash = response.data.content[0].detail
-              }
-            )
+      if (app.isPositiveInt(minfee) && app.isPositiveInt(fee)) {
+        axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
+          function(response) {
+            console.log(response.data)
+            app.accountinfo.txcount = response.data.txCnt
+            if (response.data.isRoot) {
+              axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${3}/${app.minfee.minfee}/${app.minfee.fee}/${app.accountinfo.txcount}`).then(
+                function(response) {
+                  $("#myModal").modal()
+                  console.log(response.data);
+                  app.accountinfo.txhash = response.data.content[0].detail
+                }
+              )
+            }
           }
-        }
-      )
-
+        )
+      } else {
+        alert("Values are not positive integers!");
+      }
     },
     changeBlockInterval: function(blockinterval, fee) {
       console.log("Changing block interval: ", blockinterval, fee)
-
-      axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
-        function(response) {
-          console.log(response.data)
-          app.accountinfo.txcount = response.data.txCnt
-          if (response.data.isRoot) {
-            axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${4}/${blockinterval.blockinterval}/${app.blockinterval.fee}/${app.accountinfo.txcount}`).then(
-              function(response) {
-                $("#myModal").modal()
-                console.log(response.data);
-                app.accountinfo.txhash = response.data.content[0].detail
-              }
-            )
+      if (app.isPositiveInt(blockinterval) && app.isPositiveInt(fee)) {
+        axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
+          function(response) {
+            console.log(response.data)
+            app.accountinfo.txcount = response.data.txCnt
+            if (response.data.isRoot) {
+              axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${4}/${app.blockinterval.blockinterval}/${app.blockinterval.fee}/${app.accountinfo.txcount}`).then(
+                function(response) {
+                  $("#myModal").modal()
+                  console.log(response.data);
+                  app.accountinfo.txhash = response.data.content[0].detail
+                }
+              )
+            }
           }
-        }
-      )
-
+        )
+      } else {
+        alert("Values are not positive integers!");
+      }
     },
     changeBlockReward: function(blockreward, fee) {
-      console.log("Changing blockreward: ", blockreward, fee)
-
-      axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
-        function(response) {
-          console.log(response.data)
-          app.accountinfo.txcount = response.data.txCnt
-          if (response.data.isRoot) {
-            axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${5}/${blockreward.blockreward}/${app.blockreward.fee}/${app.accountinfo.txcount}`).then(
-              function(response) {
-                $("#myModal").modal()
-                console.log(response.data);
-                app.accountinfo.txhash = response.data.content[0].detail
-              }
-            )
+      console.log("Changing block reward: ", blockreward, fee)
+      if (app.isPositiveInt(blockreward) && app.isPositiveInt(fee)) {
+        axios.get('http://192.41.136.199:8001/account/' + app.accountinfo.rootpublickey).then(
+          function(response) {
+            console.log(response.data)
+            app.accountinfo.txcount = response.data.txCnt
+            if (response.data.isRoot) {
+              axios.post(`http://192.41.136.199:8001/createConfigTx/${0}/${5}/${app.blockreward.blockreward}/${app.blockreward.fee}/${app.accountinfo.txcount}`).then(
+                function(response) {
+                  $("#myModal").modal()
+                  console.log(response.data);
+                  app.accountinfo.txhash = response.data.content[0].detail
+                }
+              )
+            }
           }
-        }
-      )
-
+        )
+      } else {
+        alert("Not all values are positive integers!");
+      }
     },
   },
   data: {
@@ -146,8 +159,6 @@ var app = new Vue({
       txhash: '',
       txcount: ''
     },
-
-    title: 'Change System Parameters'
   },
   delimiters: ["<%","%>"]
 })

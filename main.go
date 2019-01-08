a8b9085cc00236ed40403f24618d08b7d43d4da9
mchetelat <)7BWmeDNuakxj*g@>
2019-01-08 17:31:03 +0100

    Adapt to miner

--- main.go
+++ main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-  "os"
-  "github.com/bazo-blockchain/bazo-block-explorer/router"
-  "github.com/bazo-blockchain/bazo-block-explorer/data"
-  "fmt"
-  "net/http"
+	"fmt"
+	"github.com/bazo-blockchain/bazo-block-explorer/data"
+	"github.com/bazo-blockchain/bazo-block-explorer/router"
+	"github.com/bazo-blockchain/bazo-miner/p2p"
+	"net/http"
+	"os"
 )
 
 func main() {
-  if len(os.Args) == 5 {
-    requestRouter := router.InitializeRouter()
+	p2p.InitLogging()
 
-    //drops all tables in the database and creates them again
-    data.SetupDB(os.Args[3], os.Args[4])
+	if len(os.Args) == 5 {
+		requestRouter := router.InitializeRouter()
 
-    if os.Args[1] == "data" {
-      //retrieves data from the blockchain and stores it in the database
-      go data.RunDB()
-    }
-    //starts the router
-    fmt.Println("Listening...")
-    http.ListenAndServe(os.Args[2], requestRouter)
+		//drops all tables in the database and creates them again
+		data.SetupDB(os.Args[3], os.Args[4])
 
-  } else {
-    fmt.Println("Incorrect number of arguments")
-    fmt.Println("./BazoBlockExplorer <<data or nodata>> <<:WEB_PORT>> <<db_username>> <<db_password>>")
-  }
+		if os.Args[1] == "data" {
+			//retrieves data from the blockchain and stores it in the database
+			go data.RunDB()
+		}
+		//starts the router
+		fmt.Println("Listening...")
+		http.ListenAndServe(os.Args[2], requestRouter)
+
+	} else {
+		fmt.Println("Incorrect number of arguments")
+		fmt.Println("./BazoBlockExplorer <<data or nodata>> <<:WEB_PORT>> <<db_username>> <<db_password>>")
+	}
 }
